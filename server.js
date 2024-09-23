69ad764af07b91f49b9562a85eb5b00280334aaf
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-09-23 14:50:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/screenshot', async (req, res) => {
     const $url = req.query.url;
